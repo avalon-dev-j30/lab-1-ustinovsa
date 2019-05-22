68db5cda2d2e2f76d8800c6b1d2d67727e048fa3
Sergey Ustinov <48486344+ustinovsa@users.noreply.github.com>
2019-05-22 23:38:14 +0300

    Корректировка: -внесены проверки на вводимые int поля(проверка на неотрицательный ввод) -внесены проверки на вводимые double поля (проверка на неотрицательный ввод и проверка максимального значение не более 10^10) -проверка максимальной длины поля int(10) не проводится ввиду вместимости типа данных Integer (максимальное число 2147683648, что меньше 10^10)

--- src/ru/avalon/java/j30/labs/script.sql
+++ src/ru/avalon/java/j30/labs/script.sql
@@ -27,7 +27,12 @@ create table Products
     retail_value double not null,
     constraint uk_id_products unique(id),
     constraint pk_products primary key(code),
-    constraint fk_supplier_products foreign key (supplier) references Supplier(id)
+    constraint fk_supplier_products foreign key (supplier) references Supplier(id),
+    constraint ch_supplier_products check (supplier>0),
+    constraint ch_initial_price_products check
+    (initial_price>0 and initial_price<10000000000),
+    constraint ch_retail_calue_products check
+    (retail_value>0 and retail_value<10000000000)
     );
 
 create table UserInfo
@@ -35,7 +40,7 @@ create table UserInfo
     id int generated always as identity(start with 1 , increment by 1),
     name varchar(255),
     surname varchar(255),
-    constraint pk_id_userinfo primary key(id)
+    constraint pk_id_userinfo primary key(id),
     );
 
 create table Roles
@@ -44,6 +49,7 @@ create table Roles
     name varchar(255) not null,
     constraint uk_id_roles unique(id),
     constraint pk_name_roles primary key(name)
+    
     );
 
 create table Users
@@ -56,8 +62,10 @@ create table Users
     constraint pk_users primary key(email),
     constraint uk_id_users unique(id),
     constraint uk_info_users unique(info),
+    constraint ch_info_users check (info>0),
     constraint fk_info_users foreign key (info) references UserInfo(id),
-    constraint fk_role_users foreign key (roles) references Roles(id)
+    constraint fk_roles_users foreign key (roles) references Roles(id),
+    constraint ch_roles_users check (roles>0)
     );
 
 create table Orders
@@ -66,8 +74,7 @@ create table Orders
     users int not null,
     created timestamp not null,
     constraint pk_orders primary key (id),
-    constraint fk_users_orders foreign key (users)
-    references Users(id)
+    constraint fk_users_orders foreign key (users) references Users(id),
     );
 
 create table Orders2Products
@@ -78,7 +85,9 @@ create table Orders2Products
     constraint fk_orders_order2products foreign key (orders)
     references Orders(id),
     constraint fk_products_order2products foreign key (products)
-    references Products(id)
+    references Products(id),
+    constraint ch_orders_orders2products check (orders>0),
+    constraint ch_products_orders2products check (products>0)
     );
 
 insert into Supplier(name, phone) values 
@@ -87,9 +96,9 @@ insert into Supplier(name, phone) values
     ('Mitsubishi Electric', '+7(812)4495134');
 insert into Products(code, title, supplier, initial_price, retail_value) values
     ('10554', 'Compact AHU', 1, 10123.34, 12091.65),
-    ('20554', 'Central AHU', 2, 10123.34, 12091.65),
-    ('88774', 'AC Indoor Unit', 3, 10123.34, 12091.65),
-    ('98774', 'AC Outdoor Unit', 3, 10123.34, 12091.65);
+    ('20554', 'Central AHU', 2, 35488.47, 44800.05),
+    ('88774', 'AC Indoor Unit', 3, 1123.34, 1291.65),
+    ('98774', 'AC Outdoor Unit', 3, 22123.14, 26091.55);
 insert into Roles(name) values
     ('Manager'),
     ('Private Customer'),
