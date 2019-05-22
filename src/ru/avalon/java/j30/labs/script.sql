a6de29fd1cd311a8ccd5cd162020af64873f4859
Sergey Ustinov <48486344+ustinovsa@users.noreply.github.com>
2019-05-22 23:01:35 +0300

    db выполнена -ограничения  по количеству символов на int & double data types не накладываются в Apache DB -все id сделаны как автоматически увеличивающиеся на 1, начиная с 1 -в таблицы занесено не менее чем по 3 строки; -все колонки кроме name & surname в UserInfo и address & representative в Supplier не могут быть Nullable;

--- src/ru/avalon/java/j30/labs/script.sql
+++ src/ru/avalon/java/j30/labs/script.sql
@@ -1,6 +1,114 @@
-/*
- * TODO(Студент): Опишите процесс создания базы данных
- * 1. Создайте все таблицы, согласно предложенной диаграмме.
- * 2. Опишите все необходимые отношения между таблицами.
- * 3. Добавьте в каждую таблицу минимум три записи.
- */
+drop table Orders2Products;
+drop table Orders;
+drop table Products;
+drop table Supplier;
+drop table Users;
+drop table UserInfo;
+drop table Roles;
+
+create table Supplier
+    (
+    id int generated always as identity(start with 1 , increment by 1),
+    name varchar(255) not null,
+    address varchar(255),
+    phone varchar(255) not null,
+    representative varchar(255),
+    constraint uk_id_supplier unique(id),
+    constraint pk_supplier primary key (name)
+    );
+
+create table Products
+    (
+    id int generated always as identity(start with 1 , increment by 1),
+    code varchar(255) not null,
+    title varchar(255) not null,
+    supplier int not null,
+    initial_price double not null,
+    retail_value double not null,
+    constraint uk_id_products unique(id),
+    constraint pk_products primary key(code),
+    constraint fk_supplier_products foreign key (supplier) references Supplier(id)
+    );
+
+create table UserInfo
+    (
+    id int generated always as identity(start with 1 , increment by 1),
+    name varchar(255),
+    surname varchar(255),
+    constraint pk_id_userinfo primary key(id)
+    );
+
+create table Roles
+    (
+    id int generated always as identity(start with 1 , increment by 1),
+    name varchar(255) not null,
+    constraint uk_id_roles unique(id),
+    constraint pk_name_roles primary key(name)
+    );
+
+create table Users
+    (
+    id int generated always as identity(start with 1 , increment by 1),
+    email varchar(255) not null,
+    password varchar(255) not null,
+    info int not null,
+    roles int not null,
+    constraint pk_users primary key(email),
+    constraint uk_id_users unique(id),
+    constraint uk_info_users unique(info),
+    constraint fk_info_users foreign key (info) references UserInfo(id),
+    constraint fk_role_users foreign key (roles) references Roles(id)
+    );
+
+create table Orders
+    (
+    id int generated always as identity(start with 1 , increment by 1),
+    users int not null,
+    created timestamp not null,
+    constraint pk_orders primary key (id),
+    constraint fk_users_orders foreign key (users)
+    references Users(id)
+    );
+
+create table Orders2Products
+    (
+    orders int not null,
+    products int not null,
+    constraint pk_orders2products primary key (orders, products),
+    constraint fk_orders_order2products foreign key (orders)
+    references Orders(id),
+    constraint fk_products_order2products foreign key (products)
+    references Products(id)
+    );
+
+insert into Supplier(name, phone) values 
+    ('Systemair', '+7(812)3340140'),
+    ('Swegon', '+7(812)3216121'),
+    ('Mitsubishi Electric', '+7(812)4495134');
+insert into Products(code, title, supplier, initial_price, retail_value) values
+    ('10554', 'Compact AHU', 1, 10123.34, 12091.65),
+    ('20554', 'Central AHU', 2, 10123.34, 12091.65),
+    ('88774', 'AC Indoor Unit', 3, 10123.34, 12091.65),
+    ('98774', 'AC Outdoor Unit', 3, 10123.34, 12091.65);
+insert into Roles(name) values
+    ('Manager'),
+    ('Private Customer'),
+    ('VIP Customer'),
+    ('New Customer');
+insert into UserInfo (name, surname) values
+    ('Egor', 'Yarofeev'),
+    ('John', 'Smith'),
+    ('Chris', 'Willson');
+insert into Users(email, password, info, roles) values
+    ('yarofeev@gmail.com', 'yar123', 1, 2),
+    ('smith@yahoo.com', 'deckstatea43', 2, 4),
+    ('wyrshckomich@gmail.com', 'ssflegion', 3, 3);
+insert into Orders(users, created) values
+    (1, '2019-05-22 22:36:20'),
+    (2,'2019-05-23 13:23:02'),
+    (3, '2019-05-23 16:01:49');
+insert into Orders2Products(orders, products) values
+    (1, 4),
+    (2, 3),
+    (3, 2);
+
